f008828a3b110c8f945ee8a9d968e0eadd546317
alpaco <152878962+stepkono@users.noreply.github.com>
2026-08-18 20:32:54 +0200

    scanning surface material

--- Source/ArtemisOutpost/Player/HandTools/ScanningTool.cpp
+++ Source/ArtemisOutpost/Player/HandTools/ScanningTool.cpp
@@ -3,6 +3,8 @@
 
 #include "ScanningTool.h"
 
+#include "Kismet/KismetMaterialLibrary.h"
+
 
 // Sets default values
 AScanningTool::AScanningTool()
@@ -16,11 +18,65 @@ void AScanningTool::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Muzzle = Cast<USceneComponent>(FindComponentByTag<UActorComponent>(FName("ScanningTool")));
+	if (!Muzzle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ScanningTool: Failed to get Muzzle."));
+	}
+	
+	UKismetMaterialLibrary::SetScalarParameterValue(GetWorld(), SurfaceScannerCollection, FName("ScanningRadius"), ScanningRadius);
 }
 
 // Called every frame
 void AScanningTool::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	if (!(Muzzle && SurfaceScannerCollection))
+	{
+		return; 
+	}
+	
+	ScanSurface();
 }
 
+void AScanningTool::SetScannerOn(bool bScannerOn)
+{
+	bIsScannerOn = bScannerOn;
+}
+
+void AScanningTool::ScanSurface()
+{
+	if (!bIsScannerOn)
+	{
+		ResetScanningArea(); 
+		return;
+	}
+	
+	const FVector BeginTrace = Muzzle->GetComponentLocation(); 
+	const FVector Direction = Muzzle->GetForwardVector();
+	const FVector EndTrace = BeginTrace + Direction * ScanningDistance;
+	
+	FHitResult Hit;
+	FCollisionQueryParams Params(SCENE_QUERY_STAT(ScanTrace), /*bTraceComplex=*/true, this);
+	
+	const bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, BeginTrace, EndTrace, ECollisionChannel::ECC_WorldStatic, Params);
+	if (bHit)
+	{
+		const FVector Location = Hit.Location; 
+		const FLinearColor ScanAreaCenter(Location);
+		UKismetMaterialLibrary::SetScalarParameterValue(GetWorld(), SurfaceScannerCollection, FName("ScannerFlag"), 1);
+		UKismetMaterialLibrary::SetVectorParameterValue(GetWorld(), SurfaceScannerCollection, FName("ScanAreaCenter"), ScanAreaCenter);
+	}
+	else
+	{
+		ResetScanningArea(); 
+	}
+}
+
+void AScanningTool::ResetScanningArea()
+{
+	const FLinearColor NoScan(0, 0, 0);
+	UKismetMaterialLibrary::SetVectorParameterValue(GetWorld(), SurfaceScannerCollection, FName("ScanAreaCenter"), NoScan);
+	UKismetMaterialLibrary::SetScalarParameterValue(GetWorld(), SurfaceScannerCollection, FName("ScannerFlag"), 0);
+}
--- Source/ArtemisOutpost/Player/HandTools/ScanningTool.h
+++ Source/ArtemisOutpost/Player/HandTools/ScanningTool.h
@@ -14,12 +14,36 @@ class ARTEMISOUTPOST_API AScanningTool : public AHandToolBase
 public:
 	// Sets default values for this actor's properties
 	AScanningTool();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Scanning Tool")
+	void SetScannerOn(bool bScannerOn); 
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private: 
+	void ScanSurface(); 
+	
+	void ResetScanningArea();
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Scanning Tool")
+	float ScanningRadius = 100; 
+	
+	UPROPERTY(EditAnywhere, Category = "Scanning Tool")
+	float ScanningDistance = 200;
+	
+	UPROPERTY(EditAnywhere, Category = "Scanning Tool")
+	UMaterialParameterCollection* SurfaceScannerCollection;
+	
+private: 
+	UPROPERTY()
+	bool bIsScannerOn; 
+	
+	UPROPERTY()
+	USceneComponent* Muzzle; 
 };
